52ed9f61cc6fcac29e0cf8870f22056a989d6e64
Jacob Clarke <jacob@bigfish.tv>
2015-05-25 09:37:44 +1000

    recompiled pixi to include kaleidoscope webgl effect

--- app/libs/bin/pixi.js
+++ app/libs/bin/pixi.js
@@ -22020,8 +22020,6 @@ Object.defineProperties(InvertFilter.prototype, {
 });
 
 },{"../../core":26}],100:[function(require,module,exports){
-
-
 var core = require('../../core');
 // @see https://github.com/substack/brfs/issues/25
 
@@ -22037,14 +22035,12 @@ function KaleidoscopeFilter()
 {
     core.AbstractFilter.call(this,
         // vertex shader
-        null, // fs.readFileSync(__dirname + '/kaleidoscope.vert', 'utf8'),
+        null,
         // fragment shader
-        "precision lowp float;\n\nvarying vec2 vUv;\n\nuniform float sides;\nuniform float angle;\n\nuniform sampler2D tDiffuse;\n\nvoid main(void) {\n\n   vec2 p = vUv - 0.5;\n   float r = length(p);\n   float a = atan(p.y, p.x) + angle;\n   float tau = 2. * 3.1416 ;\n   a = mod(a, tau/sides);\n   a = abs(a - tau/sides/2.) ;\n   p = r * vec2(cos(a), sin(a));\n   vec4 color = texture2D(tDiffuse, p + 0.5);\n   gl_FragColor = color;\n\n}",
+        "precision mediump float;\n\n// varying vec2 position;\nvarying vec2 vTextureCoord;\nuniform sampler2D uSampler;\n\nvoid main() {\n  // normalize to the center\n  // vec2 p = position - 0.5;\n  vec2 p = vTextureCoord - 0.5;\n\n  // cartesian to polar coordinates\n  float r = length(p);\n  float a = atan(p.y, p.x);\n  \n  // kaleidoscope\n  float sides = 14.;\n  float tau = 2. * 3.1416;\n  a = mod(a, tau/sides);\n  a = abs(a - tau/sides/2.);\n  \n  // polar to cartesian coordinates\n  p = r * vec2(cos(a), sin(a));\n  \n  // sample the webcam\n  vec4 color = texture2D(uSampler, p + 0.5);\n  gl_FragColor = color;\n}",
         // custom uniforms
         {
-            sides: { type: '1f', value: 1.0 },
-            angle: { type: '1f', value: 0.0 },
-            tDiffuse: {type: 't', value: null}
+            sides: { type: '1f', value: 5 }
         }
     );
 }
@@ -22070,110 +22066,9 @@ Object.defineProperties(KaleidoscopeFilter.prototype, {
         {
             this.uniforms.sides.value = value;
         }
-    },
-    angle: {
-        get: function ()
-        {
-            return this.uniforms.angle.value;
-        },
-        set: function (value)
-        {
-            this.uniforms.angle.value = value;
-        }
     }
 });
 
-
-
-
-/*
-
-// From: http://pixelshaders.com/editor/
-
-precision mediump float;
-
-varying vec2 position;
-uniform sampler2D webcam;
-
-void main() {
-  // normalize to the center
-  vec2 p = position - 0.5;
-  
-  // cartesian to polar coordinates
-  float r = length(p);
-  float a = atan(p.y, p.x);
-  
-  // kaleidoscope
-  float sides = 10.;
-  float tau = 2. * 3.1416;
-  a = mod(a, tau/sides);
-  a = abs(a - tau/sides/2.);
-  
-  // polar to cartesian coordinates
-  p = r * vec2(cos(a), sin(a));
-  
-  // sample the webcam
-  vec4 color = texture2D(webcam, p + 0.5);
-  gl_FragColor = color;
-}
-*/
-
-
-
-/*
-
-// Pinched from http://lab.weworkweplay.com/kaleidoscope/js/shaders/KaleidoShader.js
-
-THREE.KaleidoShader = {
-
-	uniforms: {
-
-		"tDiffuse": { type: "t", value: null },
-		"sides":    { type: "f", value: 6.0 },
-		"angle":    { type: "f", value: 0.0 }
-
-	},
-
-	vertexShader: [
-
-		"varying vec2 vUv;",
-
-		"void main() {",
-
-			"vUv = uv;",
-			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-		"}"
-
-	].join("\n"),
-
-	fragmentShader: [
-
-		"uniform sampler2D tDiffuse;",
-		"uniform float sides;",
-		"uniform float angle;",
-		
-		"varying vec2 vUv;",
-
-		"void main() {",
-
-			"vec2 p = vUv - 0.5;",
-			"float r = length(p);",
-			"float a = atan(p.y, p.x) + angle;",
-			"float tau = 2. * 3.1416 ;",
-			"a = mod(a, tau/sides);",
-			"a = abs(a - tau/sides/2.) ;",
-			"p = r * vec2(cos(a), sin(a));",
-			"vec4 color = texture2D(tDiffuse, p + 0.5);",
-			"gl_FragColor = color;",
-
-		"}"
-
-	].join("\n")
-
-};
-
-*/
 },{"../../core":26}],101:[function(require,module,exports){
 var core = require('../../core');
 // @see https://github.com/substack/brfs/issues/25
