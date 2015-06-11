3e03d47a5096aedc5e7944efc0c8878fc5b014ff
Jacob Clarke <jacob@bigfish.tv>
2015-06-11 12:13:27 +1000

    Added smoke to demo effects

--- app/scripts/demo_effects.js
+++ app/scripts/demo_effects.js
@@ -1,1 +1,1 @@
-demoEffects=["circles1","lines1","lsd1","rain","triangles1","outline3","outline1","triangulation1","sprinkles"];
+demoEffects=["circles1","lines1","lsd1","rain","triangles1","outline3","outline1","triangulation1","sprinkles","smoke"];
